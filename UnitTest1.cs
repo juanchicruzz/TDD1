c98db13675453b802077aacd7ff858df5617f40f
Juan Cruz Ortega <juancruzortegacoldorf@gmail.com>
2021-05-04 19:54:43 -0300

    Agregar archivos de proyecto.

---
+++ UnitTest1.cs
@@ -0,0 +1,19 @@
+using NUnit.Framework;
+
+namespace TDD1
+{
+    [TestClass]
+    public class Tests
+    {
+        [TestMethod]
+        public int Sumar(string numeros)
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
